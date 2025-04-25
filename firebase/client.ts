9b72f3b138df6734c0ac65a71309b98eb63c4a89
Gaurang Dobariya <gaurang.dobariya@superworks.net>
2025-04-25 16:07:54 +0530

    env file

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_abak3iZAHow8mthZhWjOZqNnDrJiDEQ",
-  authDomain: "prepwise-7efae.firebaseapp.com",
-  projectId: "prepwise-7efae",
-  storageBucket: "prepwise-7efae.firebasestorage.app",
-  messagingSenderId: "800478771421",
-  appId: "1:800478771421:web:c5e8949b7ebbb6c67b0334",
-  measurementId: "G-681B24GVDT"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
